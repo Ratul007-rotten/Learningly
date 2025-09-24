a8b18ce7142ee647084de532d92d408d4696ad5c
Ratul007-rotten <browsersan02@gmail.com>
2025-09-24 13:50:53 -0600

    formulario de contacto

--- src/pages/contacto.jsx
+++ src/pages/contacto.jsx
@@ -1,12 +1,55 @@
-import { useClasses } from '../context/ClassesContext'
-import './misclases.css'
+import { useState } from 'react'
+import './contacto.css'
 
-export default function MisClases({ sidebarOpen }) {
-  const { myClasses } = useClasses()
+export default function Contacto({ sidebarOpen }) {
+
+   const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    subject: '',
+    message: ''
+  })
+
+    const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+
+    const handleSubmit = async (e) => {
+    e.preventDefault()
+    
+    const res = await fetch('http://localhost:4000/contact', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formData)
+    })
+
+    const data = await res.json()
+    console.log(data) // ver respuesta del servidor
+  }
 
   return (
-    <div className={`misclases-container ${sidebarOpen ? 'sidebar-open' : ''}`}>
-      <h1>Contacto</h1>
+    <div className={`contacto-container ${sidebarOpen ? 'sidebar-open' : ''}`}>
+      <h1>Formulario de Contacto</h1>
+      <p>Si tienes alguna pregunta o necesitas más información, no dudes en contactarnos a través del siguiente formulario:</p>
+      <form className="contact-form">
+        <label>
+          Nombre:
+          <input type="text" name="name" id='name' value={formData.name} onChange={handleChange} required />
+        </label>
+        <label>
+          Correo Electrónico:
+          <input type="email" name="email" id='email' value={formData.email} onChange={handleChange} required />
+        </label>
+        <label>
+          Asunto:
+          <input type="text" name="subject" id='subject' value={formData.subject} onChange={handleChange} required />
+        </label>
+        <label>
+          Mensaje:
+          <textarea name="message" id='message' rows="5" value={formData.message} onChange={handleChange} required></textarea>
+        </label>
+        <button type="submit" onSubmit={handleSubmit}>Enviar</button>
+      </form>
       
     </div>
   )
